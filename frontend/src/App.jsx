d6ceae79b796659f8ee897d9aa548dffa61c24b1
Mohammad Abbasi <mxabbasi@gmail.com>
2024-11-13 09:27:49 -0800

    Created User and Admin backend side and home page frontend side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Outlet } from "react-router-dom"
+import Navigation from "./pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+
+
 function App() {
   return (
     <>
-    <h1 className="text-6xl font-thin ">
-      Hello Mohammad
-    </h1>
+    <ToastContainer />
+    <Navigation />
+    <main className="py-3">
+      <Outlet />
+    </main>
   </>
   )
    
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css"
+import { Route, RouterProvider, createRoutesFromElements } from "react-router"
+import { createBrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} />
+  )
+)
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
